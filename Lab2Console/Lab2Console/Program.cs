4ce77c3eab46b5bd351296512efd8f5fd83d244b
Andrii Skrypai <Andrii.Skrypai@miratechgroup.com>
2020-01-05 21:28:02 +0200

    Lab 2 - change console buffer size and background color

--- Lab2Console/Lab2Console/Program.cs
+++ Lab2Console/Lab2Console/Program.cs
@@ -6,7 +6,25 @@ namespace Lab2Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Лабораторная работа №2. Консоль, вариант 11");
+
+            DisplayInfo();
+
+            var color = System.ConsoleColor.DarkRed;
+
+            Console.BackgroundColor = color;
+            Console.SetBufferSize(300, 200);
+
+            DisplayInfo();
+        }
+
+        static void DisplayInfo() {            
+            Console.WriteLine("Высота буфера {0} строк.", Console.BufferHeight);
+            Console.WriteLine("Ширина буфера {0} колонок.", Console.BufferWidth);
+            Console.WriteLine("Высота окна {0} строк.", Console.WindowHeight);
+            Console.WriteLine("Ширина окна {0} колонок.", Console.WindowWidth);
+            Console.WriteLine("Цвет фона {0}", Console.BackgroundColor);
         }
     }
 }
