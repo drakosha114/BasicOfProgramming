bdc5016dc88864914b0d80693639461b887ee273
Andrii Skrypai <Andrii.Skrypai@miratechgroup.com>
2020-01-09 23:20:28 +0200

    Lab 4 - part 2

--- Lab4ArrayConsole/Lab4Array-part2/Program.cs
+++ Lab4ArrayConsole/Lab4Array-part2/Program.cs
@@ -10,6 +10,79 @@ namespace Lab4Array_part2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите размерность матрицы");
+            Console.WriteLine("Введите число строк (не больше 100)");
+
+            int num_of_rows = Convert.ToInt32(Console.ReadLine());
+            /*
+            do {
+                num_of_rows = Convert.ToInt32(Console.ReadLine());
+
+            } while (num_of_rows > 0 && num_of_rows <= 100);
+            */
+            Console.WriteLine("Введите число столбцов (не больше 100)");
+
+            int num_of_colls = Convert.ToInt32(Console.ReadLine());
+            /*
+            do {
+                num_of_colls = Convert.ToInt32(Console.ReadLine());
+
+            } while (num_of_colls > 0 && num_of_colls <= 100);
+            */
+
+            int[,] arr = new int[num_of_rows, num_of_colls];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < num_of_rows; i += 1) {
+                for (int j = 0; j < num_of_colls; j += 1) {
+                    arr.SetValue(rnd.Next(), i, j);
+                }
+            }
+
+            for (int i = 0; i < num_of_rows; i += 1)
+            {
+                for (int j = 0; j < num_of_colls; j += 1)
+                {
+                    Console.Write(arr[i, j].ToString() + "; ");
+                }
+
+                Console.WriteLine("\b");
+            }
+
+            int[] minimal_Elements = new int[num_of_rows];
+
+            for (int i = 0; i < num_of_rows; i += 1)
+            {
+                int minimal_element_in_row = arr[i, 0];
+
+                for (int j = 1; j < num_of_colls; j += 1)
+                {
+                    minimal_element_in_row = arr[i, j] < minimal_element_in_row ? arr[i, j] : minimal_element_in_row;
+                }
+
+                minimal_Elements.SetValue(minimal_element_in_row, i);
+
+            }
+            Console.WriteLine("\b");
+            for (int i = 0; i < num_of_rows; i += 1)
+            {
+                Console.Write(minimal_Elements[i].ToString() + "; "); 
+            }
+            Console.WriteLine("\b");
+
+            int sumOfMinimalElements = 0;
+
+            for (int i = 0; i < num_of_rows; i += 1)
+            {
+                sumOfMinimalElements += minimal_Elements[i];
+            }
+
+            Console.WriteLine("\b");
+            Console.Write("Сумма минимальных элементов мфатрицы в строке равна: ");
+            Console.WriteLine(sumOfMinimalElements.ToString());
+            Console.WriteLine("\b");
+            Console.ReadLine();
         }
     }
 }
--- Lab4ArrayConsole/Lab4ArrayConsole/Program.cs
+++ Lab4ArrayConsole/Lab4ArrayConsole/Program.cs
@@ -2,11 +2,62 @@
 
 namespace Lab4ArrayConsole
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int arr_length = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[arr_length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr_length; i += 1)
+            {
+                int value = rnd.Next(-20, 20);
+                arr.SetValue(value, i);
+            }
+
+            Console.WriteLine("==============================");
+
+            for (int i = 0; i < arr_length; i += 1)
+            {
+                Console.Write(arr[i].ToString() + "; ");
+            }
+
+            Console.WriteLine("==============================");
+
+            Array.Sort(arr);
+
+            Console.WriteLine("========= Sorted Array Start ============");
+
+            for (int i = 0; i < arr_length; i += 1)
+            {
+                Console.Write(arr[i].ToString() + "; ");
+            }
+
+            Console.WriteLine("========= Sorted Array End ==============");
+
+            int current_index = 0;
+            int[] newArr = new int[arr_length];
+
+            for (int i = 0; i < arr_length; i += 1) {
+
+                if (arr[i] != 0) {
+                    newArr.SetValue(arr[i], current_index);
+                    current_index += 1;
+                }
+            }
+
+            Console.WriteLine("========= Filtred Array Start ============");
+
+            for (int i = 0; i < arr_length; i += 1)
+            {
+                Console.Write(newArr[i].ToString() + "; ");
+            }
+
+            Console.WriteLine("========= Filtred Array End ==============");
+
+            var a = Console.ReadLine();
         }
     }
 }
